4e63dfd10b13c8d197842cccd5be6630cb83da89
Liushuhua <2274158110@qq.com>
2017-12-22 08:20:39 +0800

    新增constants、Enum、vo类，和增加用户的DAO、Service、Mapper文件

---
+++ p2p/src/main/java/com/gs/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.gs.dao;
+
+import com.gs.bean.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Administrator on 2017/12/20
+ */
+@Repository
+public interface UserDAO extends BaseDAO {
+
+    User getByPhonePwd(@Param("phone") String phone, @Param("upwd") String upwd);
+}
---
+++ p2p/src/main/java/com/gs/service/UserService.java
@@ -0,0 +1,11 @@
+package com.gs.service;
+
+import com.gs.bean.User;
+
+/**
+ * Created by Administrator on 2017/12/21
+ */
+public interface UserService extends BaseService {
+
+    User getByPhonePwd(String phone, String upwd);
+}
