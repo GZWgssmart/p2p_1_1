7a402608c44c937718f21045c6b78036f756fe36
Liushuhua <2274158110@qq.com>
2018-01-22 19:51:12 +0800

    解决注册是推荐码框无法弹出的bug

--- p2p/src/main/webapp/static/resources/front/v01/scripts/regist.js
+++ p2p/src/main/webapp/static/resources/front/v01/scripts/regist.js
@@ -2,22 +2,15 @@ var isPhoneRegist, isUserRegist, isSend = false;
 var localObj = window.location;
 var basePath = localObj.protocol + "//" + localObj.host;
 $(function(){
-	/*
-
-
-
-
-
-
-
-
-
-
-	utils.initInput();
-	if(utils.getUrlParam('useCode')){
-		$('.from-ext').attr('class','from fadeOutUpBig');
-		$('#useCode').val(utils.getUrlParam('useCode')).attr('disabled',true);
-	};
+    var useCode = $("#useCode").val();
+    if(useCode !=''){
+        $("#asda").show();
+        $("#useCode").attr("readOnly","true");
+    }
+    $(".icon-bottom").click(function(){
+        $("#asda").show();
+    })
+    utils.initInput();
 	/*
 	var phone;
 	$('.code').one('click',function(){
